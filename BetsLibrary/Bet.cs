5cf275eed050cc991c3ebd1697107939e7e0591a
Dima <not.happy.be.happy@gmail.com>
2017-07-07 15:33:09 +0300

    Update Bet.cs

--- BetsLibrary/Bet.cs
+++ BetsLibrary/Bet.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,7 +33,9 @@ namespace BetsLibrary
         Second,
         FirstOrDraw,
         FirstOrSecond,
-        SecondOrDraw
+        SecondOrDraw,
+        P1,
+        P2
     }
 
     public class ResultBet : Bet
